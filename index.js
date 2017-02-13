9a834c243545a7de07c0c560331d942e89bccc84
Adrian Wenceslao <awenceslao@student.neumont.edu>
2017-02-13 09:52:59 -0700

    2-13-17 #3

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ path = require('path');
 var app = express();
 
 app.set('view engine', 'pug');
+app.set('views', __dirname + 'views');
 app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/',  );
 
 app.listen(3000);
