38264f03c7ca6bf54a99f54fa73c75f011b8e16c
adam.hunting <adam.hunting@gmail.com>
2017-02-21 11:40:49 -0700

    added css

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ app.get('/', function(req, res){
     res.render('index');
 } );
 
-<<<<<<< HEAD
+
  app.get('/:viewname', function(req, res){
      res.render(req.params.viewname, menu);
  });
 
-=======
+
 app.get('/:viewname', function(req, res){
     res.render(req.params.viewname, menu);
 });
->>>>>>> e72d2d9257ef8fb290468684514f376d1a5b588d
+
 
 
 app.listen(3000);
